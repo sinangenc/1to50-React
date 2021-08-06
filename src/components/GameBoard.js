7b07d77a2f13652f97ab190f5572276b72109e3c
Sinan Genc <sg32@students.kiron.ngo>
2021-08-07 01:09:32 +0200

    Dynamic determination of numbers added

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,15 +1,89 @@
 import React from 'react';
 import Square from "./Square";
+import {createNumbersArray} from "../utils/Utils";
 
 class GameBoard extends React.Component{
+
+    constructor(props) {
+        super(props);
+
+        // Get numbers as shuffled list
+        const numbers = createNumbersArray();
+
+        // Set state
+        this.state = {
+            currentNumber: 1,
+            numbers: numbers
+        }
+
+        // Binding this
+        this.handleSquareOnclick = this.handleSquareOnclick.bind(this);
+    }
+
+    handleSquareOnclick(clickedNumber){
+        //console.log("Clicked:", clickedNumber);
+
+        // If current number clicked, set it as found
+        // increase current number
+        if(this.state.currentNumber === clickedNumber){
+            this.setState((oldState) => {
+                // Iterate numbers and update isFound value of clicked number
+                const numbers = oldState.numbers.map((item) => {
+                    if(item.value === clickedNumber){
+                        item.isFound = true;
+                    }
+                    return item;
+                });
+
+                return {
+                    currentNumber: oldState.currentNumber + 1,
+                    numbers: numbers,
+                };
+            });
+        }
+    }
+
+
+    getNthNumber(n){
+        let number = null;
+
+        // If n.th number between 1-25 is not found, take it
+        if(!this.state.numbers[n].isFound){
+            number = this.state.numbers[n];
+        }
+        else if(!this.state.numbers[n + 25].isFound){
+            // If (n+25).th number is not found, take it
+            number = this.state.numbers[n + 25];
+        }
+
+        return number;
+    }
 
     render() {
         // Define a variable to store squares
         const squareArray = [];
 
-        // Add squares to list
-        for (let i = 1; i <= 25; i++){
-            squareArray.push(<Square value={i} />);
+        // Determine 25 numbers to be displayed
+        for (let i = 0; i < 25; i++){
+            const item = this.getNthNumber(i);
+
+            if(item){
+                squareArray.push(
+                    <Square
+                        key={i}
+                        value={item.value}
+                        handleOnclick={this.handleSquareOnclick}
+                    />
+                );
+            }else {
+                squareArray.push(
+                    <Square
+                        key={i}
+                        value=""
+                    />
+                );
+            }
+
         }
 
         return(
