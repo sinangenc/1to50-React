a7d81d87ee5199ad9d580713f01c3baf8678dc4f
Sinan Genc <sg32@students.kiron.ngo>
2021-08-09 23:34:27 +0200

    Css improvements for mobile devices

--- src/components/CurrentNumber.js
+++ src/components/CurrentNumber.js
@@ -4,7 +4,7 @@ class CurrentNumber extends React.Component{
     render() {
         return(
             <div className="CurrentNumber">
-                <span className="text">Current Number</span>
+                <span className="text"><span className="hideSmallScreen">Current</span> Number</span>
                 <div className="block">{this.props.currentNumber}</div>
             </div>
         );
