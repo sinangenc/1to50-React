22a66c7b717b0522038def28cf3cbadf978532ee
Sinan Genc <sg32@students.kiron.ngo>
2021-08-17 01:09:28 +0200

    Calculation of the score updated

--- src/components/FinishMessage.js
+++ src/components/FinishMessage.js
@@ -3,7 +3,7 @@ function FinishMessage(props) {
         <div className="FinishMessage">
             <h2>Congratulations!</h2>
             <h3>You have completed the game. Your Score:</h3>
-            <h2 className="Score">{props.elapsedTime}</h2>
+            <h2 className="Score">{100 - props.elapsedTime}</h2>
         </div>
     );
 }
